f8f9dc5da9b1a513e8b8fbf2c1ff7fdd46658da0
seungyong <44765636+seungyong@users.noreply.github.com>
2021-02-04 16:58:11 +0900

    text 변수 선언

--- app/src/main/java/com/ksy/corona/MainActivity.kt
+++ app/src/main/java/com/ksy/corona/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ksy.corona
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var textEle: TextView = findViewById(R.id.text)
     }
 }
